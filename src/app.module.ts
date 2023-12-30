5d62522fad5964e32cfeabefd880507dce8667b3
Sakar Aryal <techiesakar@gmail.com>
2023-12-31 02:17:48 +0545

    feat: rate limiting added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Controller, Get, Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -12,9 +15,26 @@ class AppController {
 }
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([{
+      name: 'short',
+      ttl: 1000,
+      limit: 1,
+    }, {
+      name: 'long',
+      ttl: 60000,
+      limit: 100,
+    }])
+  ],
+
   controllers: [AppController],
-  providers: [],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
   exports: []
 })
 
--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ValidationPipe } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) { }
@@ -11,11 +13,13 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false })
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'DEVELOPER') {
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 5000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
